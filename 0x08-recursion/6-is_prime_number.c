73b902d78e39ca0df610ce65656ec46e784c2414
Morenikehji <111046369+Morenikehji@users.noreply.github.com>
2022-09-28 21:46:18 +0100

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,58 +1,36 @@
 #include "main.h"
+#include <stdio.h>
 
-int check_palindrome(char *s);
+int check_prime(int n, int i);
 
 /**
-  * is_palindrome - Returns if a string is palindrome
-  * @s: the string value to be checked
+  * is_prime_number - Returns if a number is prime
+  * @n: the number to be checked
   *
   * Return: integer value
   */
-int is_palindrome(char *s)
+int is_prime_number(int n)
 {
-	if (*s == '0')
+	return (check_prime(n, 1));
+}
+
+/**
+  * check_prime - Check if number is prime
+  * @n: the number to be checked
+  * @i: the iteration times
+  *
+  * Return: 1 for prime or 0 composite
+  */
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+
+	if (n % i == 0 && i > 1)
+		return (0);
+
+	if ((n / i) < i)
 		return (1);
 
-	return (check_palindrome(s));
-}
-
-/**
-  * check_palindrome - Check if a string is palindrome
-  * @s: the string value to be checked
-  *
-  * Return: integer value
-  */
-int check_palindrome(char *s)
-{
-	int l = _strlen_recursion(s) - 1;
-
-	if (*s == s[l])
-	{
-		s++;
-		l--;
-	}
-	else
-	{
-		
-		return (0);
-	}
-
-	return (1);
-}
-
-/**
-  * _strlen_recursion - Get the length of a string
-  * @s: the string to get the length
-  *
-  * Return: the string length
-  */
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		return (0);
-	}
-
-	s++;
-	return (_strlen_recursion(s) + 1);
+	return (check_prime(n, i + 1));
 }
