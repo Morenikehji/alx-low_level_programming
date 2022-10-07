e1001a2b97fd28827f6c289400f2596aae31d42a
Morenikehji <111046369+Morenikehji@users.noreply.github.com>
2022-10-07 11:09:53 +0100

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,63 +2,73 @@
 
 #include <stdlib.h>
 
+#include <stdio.h>
+
 /**
- * string_nconcat - ...
- * @s1: ...
- * @s2: ...
- * @n: ...
- *
- * Return: ...
- */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+  * _realloc - ...
+  * @ptr: ...
+  * @old_size: ...
+  * @new_size: ...
+  *
+  * Return: ...
+  */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+
 {
-unsigned int i = 0, j = 0, k = 0, l = 0;	
-	char *str;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
 
-	s2 = "";
+	char *nptr;	unsigned int i;
 
-	while (s1[i])
+	if (new_size == old_size)
 
-	i++;
+		return (ptr);
 
-	while (s2[k])
-
-		k++;
-
-	if (n >= k)
-
-		l = i + k;
-
-	else
-
-		l = i + n;
-
-	str = malloc(sizeof(char) * l + 1);
-
-	if (str == NULL)
-
-		return (NULL);
-
-	k = 0;
-
-	while (j < l)
+	if (ptr == NULL)
 
 	{
 
-		if (j <= i)
+		nptr = malloc(new_size);
 
-			str[j] = s1[j];
+		if (nptr == NULL)
 
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
+			return (NULL);
+
+		return (nptr);
+
 	}
-	str[j] = '\0';
-	return (str);
+
+	else
+
+	{
+
+		if (new_size == 0)
+
+		{
+
+			free(ptr);
+
+			return (NULL);
+
+		}
+
+	}
+
+	nptr = malloc(new_size);
+
+	if (nptr == NULL)
+
+		return (NULL);
+
+	for (i = 0; i < old_size && i < new_size; i++)
+
+	{
+
+		nptr[i] = ((char *) ptr)[i];
+
+	}
+
+	free(ptr);
+
+	return (nptr);
+
 }
